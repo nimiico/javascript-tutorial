d100204df3dbd9f4c8fe3863eabc7b3bc3949ca7
Nima Eslami <nimaeslami206@gmail.com>
2023-07-30 16:05:12 +0330

    Fundamentals-part1

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,105 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("Nima");
+// console.log(23);
+
+// let firstName = "Nima";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof js);
+
+// let year;
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// const age = 2037 - 2000;
+// console.log(age);
+
+// console.log("Nima" + " " + "Eslami");
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// console.log(x);
+
+// const isCorrect = age > 12;
+// console.log(isCorrect);
+
+// CHALLENGE 1
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const massNima = 92;
+// const heightNima = 1.92;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMINima = massNima / (heightNima * heightNima);
+
+// console.log(BMIMark, BMINima);
+
+///////////////////////
+
+// const name = "Ali";
+// const age = 22;
+// const x = `${name} is ${age} years old.`;
+// console.log(x);
+
+//////////////////////
+// const age = 6;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//   console.log("sara can start driving licence.");
+// } else {
+//   const yearsleft = 18 - age;
+// }
+
+/////////////////////
+
+// console.log(10 + "3" + 4);
+// console.log(10 + 3 + "4");
+// console.log("23" - "10");
+// console.log("10" * "2");
+// console.log("10" / "2");
+// console.log("10" > "1");
+//////////////////////////////
+//falsy
+// const money = 0;
+
+// if (money) {
+//   console.log("Don't spend it");
+// } else {
+//   console.log("spend it");
+// }
+
+//////////////////////////
+// equality
+// console.log("18" == 18);
+// console.log(18 === "18");
+
+////////////////////////////
+// const day = "saturday";
+
+// switch (day) {
+//   case "sunday":
+//     console.log("sunday");
+//     break;
+//   case "saturday":
+//     console.log("saturday");
+//     break;
+//   default:
+//     console.log("none");
+// }
+
+///////////////////////////////
+// TERNARY
+const age = 17;
+const x = age >= 18 ? "OK" : "NO";
+console.log(age >= 18 ? "OK" : "NO");
